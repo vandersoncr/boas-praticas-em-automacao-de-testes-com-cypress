899f6573f4be7895ce30b687751bd95b5eb9adbc
Vanderson Cardoso Rodrigues <vcr@certi.org.br>
2025-04-14 17:29:11 -0400

    resolvendo exercicio duplication e flaky

--- cypress/integration/browser-testing/sample2.spec.js
+++ cypress/integration/browser-testing/sample2.spec.js
@@ -1,13 +1,11 @@
 describe('Browser testing bad practice', () => {
   beforeEach(() => {
-    cy.visit('https://hackernews-seven.vercel.app')
+    cy.visit('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
   })
 
   it('tests a browser feature instead of an app feature', () => {
-    cy.get('.table-row a')
-      .first()
-      .click()
-
-    // Assert that a new tab was opened in the correct URL
-  })
+    cy.contains('a', 'Política de Privacidade')
+      .should('have.attr', 'href', 'privacy.html')
+      .and('have.attr', 'target', '_blank')
+   })
 })
